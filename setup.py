2088c35452ff73cfe0fc527c6d4f73d80ccd1018
David <david.figueroa.alejandro@gmail.com>
2017-10-22 18:39:16 +0200

    Correcciones instalacion

--- setup.py
+++ setup.py
@@ -1,28 +1,57 @@
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = '0.0.6'
 
 setup(
     name='ckanext-iaest',
     version=version,
-    description="Aragon Opendata IAEST Harvester for CKAN",
-    long_description="",
-    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    description="Plugins for exposing and consuming IAEST metadata on CKAN",
+    long_description='''\
+    ''',
+    classifiers=[],
     keywords='',
-    author='dFigue - Aragon Opendata',
-    author_email='david.figueroa.alejandro@gmail.com',
-    url='https://github.com/dFigue/ckanext-iaest.git',
+    author='Open Knowledge Foundation',
+    author_email='info@ckan.org',
+    url='https://github.com/okfn/ckanext-iaest',
     license='AGPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.iaest'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'ckanext-harvest'
+        # -*- Extra requirements: -*-
     ],
-    entry_points="""
-        [ckan.plugins]
-        iaeset=ckanext.iaest.plugin:IAESTPlugin
-        iaest_harvester=ckanext.iaest.harvester:IAESTHarvester        
-        """,
+    entry_points='''
+
+    [ckan.plugins]
+    iaest_xml_harvester=ckanext.iaest.harvesters:IAESTXMLHarvester
+    iaest_json_harvester=ckanext.iaest.harvesters:IAESTJSONHarvester
+
+    iaest_rdf_harvester=ckanext.iaest.harvesters:IAESTRDFHarvester
+
+    iaest_json_interface=ckanext.iaest.plugins:IAESTJSONInterface
+
+    iaest=ckanext.iaest.plugins:IAESTPlugin
+
+    # Test plugins
+    test_rdf_harvester=ckanext.iaest.tests.test_harvester:TestRDFHarvester
+    test_rdf_null_harvester=ckanext.iaest.tests.test_harvester:TestRDFNullHarvester
+    test_rdf_exception_harvester=ckanext.iaest.tests.test_harvester:TestRDFExceptionHarvester
+
+    [ckan.rdf.profiles]
+    euro_iaest_ap=ckanext.iaest.profiles:EuropeanIAESTAPProfile
+
+    [paste.paster_command]
+    generate_static = ckanext.iaest.commands:GenerateStaticIAESTCommand
+
+    [babel.extractors]
+    ckan = ckan.lib.extract:extract_ckan
+    ''',
+    message_extractors={
+        'ckanext': [
+            ('**.py', 'python', None),
+            ('**.js', 'javascript', None),
+            ('**/templates/**.html', 'ckan', None),
+        ],
+    },
 )
